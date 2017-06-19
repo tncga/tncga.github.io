f546fbc8e34bec58494aba3298213e66b0d68589
tncga <qokberk_akdeniz@hotmail.com>
2017-06-19 21:44:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
 function searchQuery(input) {
 	if (input=="google") {
 		window.location.href="https://www.google.com.tr/search?q="+document.getElementById('q_google').value;
@@ -24,6 +38,27 @@ var stats = angular.module("csgostats", []);
 stats.controller("ctrl", function ($scope) {
 	$scope.curl = "http://csgo-stats.com/"+id;
 	$scope.cpng = "http://csgo-stats.com/"+id+"/graphic.png";
-	$scope.zurl = "http://rank.sunucuadresi.com/playerinfo/"+zid;
-	$scope.zpng = "http://rank.sunucuadresi.com/sig/"+zid+"_9.png";	
+	//$scope.zurl = "http://rank.sunucuadresi.com/playerinfo/"+zid;
+	//$scope.zpng = "http://rank.sunucuadresi.com/sig/"+zid+"_9.png";	
 });
+
+var client = getParameterByName("client_id")
+
+function spotifyCode() {
+window.open("https://accounts.spotify.com/authorize/?client_id=b1e7787ac8764ebb8f78c3881bbb9094&response_type=code&redirect_uri=https://tncga.github.io/youtify.html?id=ok&state=3f5Dd5as", "_blank", "width=400,height=500")
+}
+
+function spotifyToken(code) {
+var http = new XMLHttpRequest();
+var url = "POST https://accounts.spotify.com/api/token";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Authorization", "Basic " + client);
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send("grant_type=authorization_code&code=" + code + "&redirect_uri=https://tncga.github.io/youtify.html?id=ok");
+}
